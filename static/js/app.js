f3c553a84e52041bd0b6f7e1a8cce515fbbe8926
Jacqueline Hamlin <jaquelona@Jacquelines-MacBook-Pro.local>
2020-05-11 17:49:30 -0400

    working

--- static/js/app.js
+++ static/js/app.js
@@ -1,44 +1,45 @@
 // from data.js
-var tableData = data;
+//var tableData = data;
 var tbody = d3.select("tbody");
 // YOUR CODE HERE!
 
-data.forEach((tableData) => {
+function printdata(tableData) {
+  tbody.html("")
+  tableData.forEach((datarow) => {
     var row = tbody.append("tr");
-    Object.entries(tableData).forEach(([key, value]) => {
+    Object.entries(datarow).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     })
-});
-
-// Select button on form for date filtering
-var button = d3.select("#filter-btn");
-// Select form used for entering date to filter
-
-var form = d3.select("#form");
-// Create event handlers for clicking the button to filter by date
-// or by pressing enter
-button.on("click", runEnter);
-//form.on("submit", runEnter);
-// Create funcion to run for both clicking the button and pressing enter
-function runEnter() {
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-  // Define variable for input date and select id that stores the date
-  var inputDate = d3.select("#datetime");
-  // Define variable for value property of input date
-  var inputValue = inputDate.property("value");
-  console.log(inputValue)
-  var filteredData = tableData.filter(info => info.datetime === inputValue);
-  console.log(filteredData)};
-  //var tableData = filteredData
-  var tbody = d3.select("tbody");
-// YOUR CODE HERE!
-  tbody.html("")
-  filteredData.forEach((tableData) => {
-      var row = tbody.append("tr");
-      Object.entries(tableData).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
-      })
   });
+}
+printdata(data)
+
+//Select button on form for date filtering
+var button = d3.select("#filter-btn");
+//Select form used for entering date to filter
+
+// // Create event handlers for clicking the button to filter by date
+// // or by pressing enter
+button.on("click", runEnter);
+
+// // Create funcion to run for both clicking the button and pressing enter
+function runEnter() {
+  var input = d3.select("#datetime").property("value");
+  console.log(input);
+  var filteredData = data.filter (row => row.datetime === input);
+  console.log(filteredData)
+  printdata(filteredData)
+};
+
+  //   //var tableData = filteredData
+//   var tbody = d3.select("tbody");
+// // YOUR CODE HERE!
+//   tbody.html("")
+//   filteredData.forEach((tableData) => {
+//       var row = tbody.append("tr");
+//       Object.entries(tableData).forEach(([key, value]) => {
+//         var cell = row.append("td");
+//         cell.text(value);
+//       })
+//   });
